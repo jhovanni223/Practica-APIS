dd00a211e77a933ef77c93567800d43b31eeb77e
AlejandroOrozco456 <alejandrogobass@gmail.com>
2022-02-14 00:46:38 -0600

    Cambios a mi Card Personal

---
+++ js/Sleepy_Gary.js
@@ -0,0 +1,19 @@
+const URL_API="https://rickandmortyapi.com/api/character/324";
+
+
+var contenido = document.querySelector('#contenido')
+function fetchRick(){
+    fetch(URL_API)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        contenido.innerHTML=`
+        <img src= "${data.image}">
+        <p> Id: ${data.id} </p>
+        <p> Género ${data.gender} </p>
+        <p> Nombre: ${data.name} </p>
+        <p> Especie: ${data.species} </p>
+        `
+    })
+}
+fetchRick(1);
