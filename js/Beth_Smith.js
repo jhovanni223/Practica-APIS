be767b49f6ae60ba81b39aa6be775333ebb4bbb5
unknown <abril.hernandez.hernandez.02@gmail.com>
2022-02-14 14:51:52 -0600

    Card Personaje

---
+++ js/Beth_Smith.js
@@ -0,0 +1,28 @@
+const urlapir='https://rickandmortyapi.com/api/character/4';
+
+fetch(urlapir)
+.then(respuesta => respuesta.json())
+.then(data =>{
+    
+    let elemento=document.getElementById("Beth");
+    console.log(data.name);
+    elemento.innerHTML =`
+    <link rel="stylesheet" href="../../css/estilo.css">
+    <div class="cuerpo">
+    <div class="card">
+        <div class="card-image">
+            <img src='${data.image}'>
+        </div>
+        <div class="card-text">
+            <br>
+        <h2 id="nombre" >${data.name}</h2>
+        <h3 >Gender</h3>
+        <p id="gender">${data.gender}</p>
+        <h3 >Species</h3>
+        <p id="specie">${data.spicies}</p>         
+        </div>  
+        </div>
+    </div>
+    `;
+})
+.catch(error=>console.log(error))
