5660de68181b4d425a1f052c74e2d393c4b1ea3f
unknown <mariana.avilesmartinez@gmail.com.com>
2022-02-14 23:22:06 -0600

    Se agregó personaje Rick

---
+++ js/Squanchy.js
@@ -0,0 +1,28 @@
+const urlapir='https://rickandmortyapi.com/api/character/331';
+
+fetch(urlapir)
+.then(respuesta => respuesta.json())
+.then(data =>{
+    
+    let elemento=document.getElementById("Squanchy");
+    console.log(data.name);
+    elemento.innerHTML =`
+    <link rel="stylesheet" href="../../css/estilo.css">
+    <div class="cuerpo">
+    <div class="card">
+        <div class="card-image">
+            <img src='${data.image}'>
+        </div>
+        <div class="card-text">
+            <br>
+        <h2 id="nombre" >${data.name}</h2>
+        <h3 >Gender</h3>
+        <p id="gender">${data.gender}</p>
+        <h3 >Species</h3>
+        <p id="specie">${data.species}</p>         
+        </div>  
+        </div>
+    </div>
+    `;
+})
+.catch(error=>console.log(error))
